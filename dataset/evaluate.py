d7da0eee319202eeaf7d9c646b809cd1043d9533
unknown <shubhamdave70@gmail.com>
2022-10-23 22:52:56 -0400

    added task 3

--- dataset/evaluate.py
+++ dataset/evaluate.py
@@ -1,13 +1,14 @@
 import csv
 import sys
 
+
 def main(file):
     TP = 0
     FP = 0
     TN = 0
     FN = 0
 
-    with open(file, "r") as t, open("test-key.txt", "r") as k:
+    with open(file, "r") as t, open("test.txt", "r") as k:
         kr = list(csv.reader(k, delimiter=" "))
         tr = list(csv.reader(t, delimiter=" "))
         for i in range(58):
@@ -32,6 +33,7 @@ def main(file):
     print("Precision: {}".format(PREC))
     print("Recall: {}".format(REC))
     print("F1: {}".format(F1))
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
